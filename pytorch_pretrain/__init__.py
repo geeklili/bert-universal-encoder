bdb6254fe3339329f1148c427f09e0722dab72d2
geeklili <netgeek@163.com>
2020-11-13 17:14:36 +0800

    add bert-adam

--- pytorch_pretrain/__init__.py
+++ pytorch_pretrain/__init__.py
@@ -1,4 +1,5 @@
 __version__ = "0.6.2"
 from .tokenization import BertTokenizer, BasicTokenizer, WordpieceTokenizer
 from .modeling import BertModel
+from .optimization import BertAdam
 from .file_utils import PYTORCH_PRETRAINED_BERT_CACHE, cached_path, WEIGHTS_NAME, CONFIG_NAME
